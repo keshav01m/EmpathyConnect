ef4707a59acecf306171ab1dfabfeaa9cd8fd657
keshav01m <keshav01m@gmail.com>
2024-10-11 09:47:34 +0530

    completed

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,9 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
-const app = express();
+import { app, server } from "./socket/socket.js";
+
+
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -23,7 +25,7 @@ app.use("/api/users", userRoutes);
 //   res.send("Hello World");
 // });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`server is running on port ${PORT}`);
 });
